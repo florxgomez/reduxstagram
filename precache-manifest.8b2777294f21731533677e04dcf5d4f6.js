b9d98c11fd6fd7edf25bb12cb8b74cf4843a7e8a
Flor <florxgomez@gmail.com>
2019-10-29 18:53:24 -0300

    Updates

---
+++ precache-manifest.8b2777294f21731533677e04dcf5d4f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc0f0c166c56b22a2e75dc87efab77d",
+    "url": "/reduxstagram/index.html"
+  },
+  {
+    "revision": "3c678a947b9f2b477904",
+    "url": "/reduxstagram/static/css/main.1860b041.chunk.css"
+  },
+  {
+    "revision": "6e7e2ce88ef03ffc59a3",
+    "url": "/reduxstagram/static/js/2.97898c50.chunk.js"
+  },
+  {
+    "revision": "3c678a947b9f2b477904",
+    "url": "/reduxstagram/static/js/main.321bb2e4.chunk.js"
+  },
+  {
+    "revision": "da50dc848b10ff86348d",
+    "url": "/reduxstagram/static/js/runtime-main.7e75e9af.js"
+  }
+]);
